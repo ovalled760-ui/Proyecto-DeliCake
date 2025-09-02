a18cf913450560793ec4d1543ba58b4c947257e7
cvargaspimentel157-star <cvargaspimentel157@gmail.com>
2025-09-02 08:24:53 -0500

    Auto-sync: file changes

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-# Esta función es requerida por Flask-Login para cargar un usuario
+
 @login_manager.user_loader
 def load_user(user_id):
     #La función debe devolver una instancia de la clase que hereda de UserMixin
