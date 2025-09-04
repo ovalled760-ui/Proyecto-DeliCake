9755c67d500d920b44ae46efcb9f8e2577ca9355
Mariana Rojas <mrojasrodriguez04@gmail.com>
2025-09-03 21:23:19 -0500

    ...

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from controladores.models import db, Usuario, Cliente
+from routes.admin import bp as admin_bp
 
 app = Flask(__name__)
 
@@ -15,6 +16,7 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
 
 
 @login_manager.user_loader
@@ -121,8 +123,10 @@ def logout():
     return redirect(url_for('publica'))
 
 
+app.register_blueprint(admin_bp)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
