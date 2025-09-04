f2c2fc9fec4c6d63cbae219d45b0a70f4cc668d9
Mariana Rojas <mrojasrodriguez04@gmail.com>
2025-09-03 21:23:19 -0500

    ...

---
+++ routes/admin.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from controladores.models import db, Producto
+import os
+
+bp = Blueprint("admin", __name__, url_prefix="/admin")
+
+@bp.route("/agregar", methods=["GET", "POST"])
+def agregar_producto():
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        descripcion = request.form["descripcion"]
+        precio = request.form["precio"]
+        descuento = request.form["descuento"]
+        categoria = request.form["categoria"]
+
+ 
+        imagen_file = request.files["imagen"]
+        if imagen_file and imagen_file.filename != "":
+            imagen_path = imagen_file.filename
+            imagen_file.save(os.path.join("static/img", imagen_path))
+        else:
+            imagen_path = "default.jpg"
+
+        nuevo = Producto(
+            nombre=nombre,
+            descripcion=descripcion,
+            precio=precio,
+            descuento=descuento,
+            categoria=categoria,
+            imagen=imagen_path
+        )
+        db.session.add(nuevo)
+        db.session.commit()
+
+        flash("Producto agregado con éxito ✅")
+        return redirect(url_for("admin.agregar_producto"))
+
+    return render_template("admin/agregar_producto.html")
+
+@bp.route("/catalogo")
+def catalogo():
+    productos = Producto.query.all()
+    
+    return render_template('catalogo.html', productos = productos)
+    
